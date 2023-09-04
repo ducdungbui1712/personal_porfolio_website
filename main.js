1126ffa13615374a55eaf162aa7c893024963577
Bui Duc Dung <tuivodoi4@gmail.com>
2023-09-04 16:05:19 +0700

    Add animation css to home header screen

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".header-text-position", {
+    strings: ['Software Developer', 'Web Developer'],
+    typeSpeed: 50,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay:1000,
+    loop:true
+    });
