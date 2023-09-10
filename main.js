0a3303d0f3735153e44d5f20bc5d82016e5ed0fe
ducdungbui <ducdungbui1712@gmail.com>
2023-09-10 21:37:24 +0700

    About me tab and Contact tab completed, working on Project tab and footer

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
-var typed = new Typed(".header-text-position", {
-    strings: ['Software Developer', 'Web Developer'],
-    typeSpeed: 50,
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay:1000,
-    loop:true
-    });
+window.onload = function(){
+    headerPositionAnimation();
+    aboutMeTabClickEvent();
+}
+
+function headerPositionAnimation() {
+    var typed = new Typed(".header-text-position", {
+        strings: ['Software Developer', 'Web Developer'],
+        typeSpeed: 50,
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay:1000,
+        loop:true
+        });
+}
+
+function aboutMeTabClickEvent(){
+    const tabLinks = document.querySelectorAll('.tab-links');
+    const tabContents = document.querySelectorAll('.tab-contents');
+
+    tabLinks.forEach((link, index) => {
+        link.addEventListener('click', () => {
+            tabLinks.forEach(link => {
+                link.classList.remove('active');
+            })
+
+            tabContents.forEach(tabContent => {
+                tabContent.classList.remove('active-tab');
+            });
+
+            link.classList.add('active');
+            tabContents[index].classList.add('active-tab');
+        })
+    })
+}
